41a299f4cdc57d7de2ca8dbe3ef45877271bd382
RENO WARNER <reno.warner.777@gmail.com>
2026-05-28 20:58:29 -0400

    FIX: Added token authentication to fetch build script.

--- netlify/fetch.js
+++ netlify/fetch.js
@@ -1,7 +1,11 @@
 import { getStore } from "@netlify/blobs";
 import fs from "fs";
 
-const store = getStore("datasets");
+const store = getStore({
+	name: "datasets",
+	siteID: process.env.NETLIFY_SITE_ID,
+	token: process.env.NETLIFY_AUTH_TOKEN,
+});
 
 const data = await store.get("coasters", { type: "text" });
 
