72981e4f48d45a58c8ed46d1d6b8eeac2dd60dbf
RENO WARNER <reno.warner.777@gmail.com>
2026-05-28 21:18:19 -0400

    FIX: Disable pino pretty for deployed.

--- netlify/functions/core/logger.ts
+++ netlify/functions/core/logger.ts
@@ -1,11 +1,12 @@
 import pino from "pino";
 
+const isLocal =
+	process.env.NETLIFY_DEV === "true" || process.env.NODE_ENV !== "production";
+
 export const logger = pino({
-	// "debug" level captures everything; change to "info" or "error" in production
-	level: process.env.LOG_LEVEL || "debug",
-	// Makes local console outputs beautiful and readable instead of raw JSON strings
-	transport:
-		process.env.NODE_ENV !== "production"
-			? { target: "pino-pretty" }
-			: undefined,
+	level: process.env.LOG_LEVEL || "info",
+
+	transport: isLocal
+		? { target: "pino-pretty", options: { colorize: true } }
+		: undefined,
 });
