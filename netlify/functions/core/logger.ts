635925d098038bcfcd9140f84b38661855dfa074
RENO WARNER <reno.warner.777@gmail.com>
2026-05-29 01:27:04 -0400

    FIX: Removed pino-pretty to fix netlify deployment.

--- netlify/functions/core/logger.ts
+++ netlify/functions/core/logger.ts
@@ -1,14 +1,5 @@
 import pino from "pino";
 
-const prodLogger = pino({ level: process.env.LOG_LEVEL || "info" });
-
-const devLogger = pino({
-	level: "debug",
-	transport: {
-		target: "pino-pretty",
-		options: { colorize: true },
-	},
+export const logger = pino({
+	level: process.env.LOG_LEVEL || "debug",
 });
-
-export const logger =
-	process.env.NODE_ENV === "development" ? prodLogger : devLogger;
